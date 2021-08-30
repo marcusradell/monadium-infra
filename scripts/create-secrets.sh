38f364ce85985d01238769cbeaa827785bcffe09
Marcus Rådell <marcus@radell.net>
2021-08-30 16:49:52 +0200

    secrets: add owner email/password secrets

--- scripts/create-secrets.sh
+++ scripts/create-secrets.sh
@@ -1,3 +1,6 @@
 kubectl create secret generic monadium-secret \
     --from-file=./secrets/DATABASE_URL \
-    --from-file=./secrets/JWT_SECRET
+    --from-file=./secrets/JWT_SECRET \
+    --from-file=./secrets/IDENTITIES_OWNER_EMAIL \
+    --from-file=./secrets/IDENTITIES_OWNER_PASSWORD
+    
